23eafd982c221e2e2367b561044fc9050814718c
brepp22 <brepp@hotmail.com>
2024-04-11 09:02:31 -0700

    updated changes to project

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import * as yup from 'yup'
+import axios from 'axios'
 
 // 👇 Here are the validation errors you will use with Yup.
 const validationErrors = {
@@ -8,6 +10,20 @@ const validationErrors = {
 }
 
 // 👇 Here you will create your schema.
+const formSchema = yup.object().shape({
+  fullName: yup
+  .string()
+  .trim()
+  .min(3, validationErrors.fullNameTooShort)
+  .max(20, validationErrors.fullNameTooLong)
+  .required(),
+  size: yup
+  .string()
+  .required()
+  .trim()
+  .oneOf(['S' , 'M', 'L'], validationErrors.sizeIncorrect),
+})
+
 
 // 👇 This array could help you construct your checkboxes using .map in the JSX.
 const toppings = [
@@ -18,44 +34,116 @@ const toppings = [
   { topping_id: '5', text: 'Ham' },
 ]
 
+
+const getInitialValues = () => ({
+  fullName: '',
+  size: '',
+  toppings: [],
+})
+
+const getInitialErrors = () => ({
+  fullName: '', 
+  size: '',
+  toppings: [],
+})
+
 export default function Form() {
+
+  const [values, setValues] = useState(getInitialValues())
+  const [errors, setErrors]  = useState(getInitialErrors())
+  const [serverSuccess, setServerSuccess] = useState()
+  const [serverFailure, setServerFailure] = useState()
+  const [formEnabled, setFormEnabled] = useState(false)
+
+
+  useEffect(() => {
+    formSchema.isValid(values).then(setFormEnabled)
+  })
+
+  const onChange = evt => {
+    let {type, id, value, checked} = evt.target
+    value = type == 'checkbox'? checked : value
+    setValues( {...values , [id] : value})
+    yup.reach(formSchema, id).validate(value)
+    .then(() => setErrors({...errors, [id]: ''}))
+    .catch((err) => setErrors({...errors, [id]: err.errors[0]}))
+  }
+
+  const changeToppings = evt => {
+    let { type, topping_id, checked, name } = evt.target
+    if (type === 'checkbox') {
+      let updatedToppings
+      if (checked) {
+        updatedToppings = [...values.toppings, name]
+      } else {
+        updatedToppings = values.toppings.filter(topping_id => topping_id !== name)
+      }
+      setValues({ ...values, toppings: updatedToppings })
+    } else {
+      const value = evt.target.value;
+      setValues({ ...values, [topping_id]: value })
+    }
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+    axios.post('http://localhost:9009/api/order', values)
+    .then(res =>{
+      setServerSuccess(res.data.message)
+      setServerFailure()
+    })
+    .catch(err => {
+      setServerFailure(err.response.data.message)
+      setServerSuccess()
+    })
+    .finally(() => {
+      setValues(getInitialValues())
+    })
+  }
+
   return (
-    <form>
+    <form onSubmit = {onSubmit}>
       <h2>Order Your Pizza</h2>
-      {true && <div className='success'>Thank you for your order!</div>}
-      {true && <div className='failure'>Something went wrong</div>}
+      {serverSuccess && <div className='success'>{serverSuccess}</div>}
+      {serverFailure && <div className='failure'>{serverFailure}</div>}
 
       <div className="input-group">
         <div>
           <label htmlFor="fullName">Full Name</label><br />
-          <input placeholder="Type full name" id="fullName" type="text" />
+          <input value={values.fullName} onChange={onChange} id="fullName" type="text" placeholder="Type full name" />
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.fullName && <div className='error'>{errors.fullName}</div>}
       </div>
 
       <div className="input-group">
         <div>
           <label htmlFor="size">Size</label><br />
-          <select id="size">
+          <select id="size" value = {values.size} onChange ={onChange}>
             <option value="">----Choose Size----</option>
-            {/* Fill out the missing options */}
+            <option value = "S">Small</option>
+            <option value = "M">Medium</option>
+            <option value = "L">Large</option>
           </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.size && <div className='error'>{errors.size}</div>}
       </div>
 
       <div className="input-group">
-        {/* 👇 Maybe you could generate the checkboxes dynamically */}
-        <label key="1">
-          <input
-            name="Pepperoni"
-            type="checkbox"
-          />
-          Pepperoni<br />
-        </label>
+      {toppings.map((topping, index) => (
+          <label key={index}>
+            <input
+              checked={values.toppings.includes(topping.topping_id)}
+              onChange={changeToppings}
+              name={topping.topping_id}
+              type="checkbox"
+            />
+            {topping.text} <br />
+            </label>
+        ))}
+        
       </div>
       {/* 👇 Make sure the submit stays disabled until the form validates! */}
-      <input type="submit" />
+      <input disabled={!formEnabled} type="submit" />
     </form>
   )
 }
--- mvpB.test.js
+++ mvpB.test.js
@@ -14,7 +14,21 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [4] sum('1', 2) // returns 3
     [5] sum('10', '3') // returns 13
   */
+  function sum(a, b) {
+    return a + b 
+  }
 
+  test('adds 1 + 3 to equal 4' , () => {
+    expect(sum(1,3)).toBe(4)
+  })
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(3)
+  })
+
+  test('adds 10 + 3 to equal 13', () => {
+    expect(sum(10,3)).toBe(13)
+  })
   /*
   👉 TASK 2 - Integration Testing of HelloWorld component at the bottom of this module
 
@@ -29,9 +43,9 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] renders a text that reads "JavaScript is pretty awesome"
     [6] renders a text that includes "javaScript is pretty" (use exact = false)
   */
-  test('you can comment out this test', () => {
-    expect(true).toBe(false)
-  })
+  // test('you can comment out this test', () => {
+  //   expect(true).toBe(false)
+  // })
 })
 
 function sum(a, b) {
